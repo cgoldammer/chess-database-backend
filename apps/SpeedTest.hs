8a5ac36f387e68b019c558c3a0efce51e2cf1c4f
cgoldammer <goldammer.christian@gmail.com>
2018-06-04 03:34:16 +0000

    Added speed test

---
+++ apps/SpeedTest.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedStrings, ScopedTypeVariables, TypeFamilies #-}
+
+module Main where
+
+import Services.Helpers
+import Services.Service
+import qualified Test.Helpers as TH
+import Database.Esqueleto hiding (get)
+import Services.DatabaseHelpers
+import Database.Persist hiding ((==.))
+import Database.Persist.Sql hiding ((==.))
+
+import Services.Types
+
+dummy = [(a, a+1) | a <- [0..10000]]
+
+main :: IO ()
+main = do
+  let grouped = groupWithVal id dummy
+  (players, evals) <- TH.inBackend (connString dbName) dataResults
+  let summ = summarizeEvals players evals
+  print $ length summ
+
+getTestEvals :: TH.DataAction [EvalResult]
+getTestEvals = do
+  er <- select $
+    from $ \(me, g) -> do
+      where_ $ (me^.MoveEvalGameId ==. g^.GameId)
+      return (me, g)
+  return er
+
+dbName :: String
+dbName = "prod"
+
+dataResults :: TH.DataAction ([Entity Player], [EvalResult])
+dataResults = do
+  dbPlayers :: [Entity Player] <- selectList [] []
+  evals <- getTestEvals
+  return (dbPlayers, evals)
